b545fffe70c1b22586640c0f7d0bc8e4ff72c415
PetDung <perdhd1702@gmail.com>
2023-11-03 00:14:58 +0700

    color entity

--- src/main/java/com/nhom_6/duan_1/model/entity/Color.java
+++ src/main/java/com/nhom_6/duan_1/model/entity/Color.java
@@ -1,4 +1,21 @@
 package com.nhom_6.duan_1.model.entity;
 
-public class Color {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Color extends BaseEntity  {
+
+    @Column
+    private String colorName;
 }
