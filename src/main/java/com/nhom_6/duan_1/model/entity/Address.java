05fe3ccfc7cf942fd485699d1f8a13f6f9ec276d
your name <nam060620041220@gmail.com>
2023-11-03 00:01:30 +0700

    dung

--- src/main/java/com/nhom_6/duan_1/model/entity/Address.java
+++ src/main/java/com/nhom_6/duan_1/model/entity/Address.java
@@ -1,4 +1,27 @@
 package com.nhom_6.duan_1.model.entity;
 
-public class Address {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address extends BaseEntity {
+    @Column
+    private String province;
+    @Column
+    private String district;
+    @Column
+    private String wards;
+    @Column
+    private String addressDetail;
+
 }
